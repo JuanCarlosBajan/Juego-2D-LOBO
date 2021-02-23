980a7030b523b1cf4665fb4c913240a82b26f6f2
juan carlos bajan <baj20109@uvg.edu.gt>
2021-02-23 16:51:41 -0600

    juego

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public Animator anim;
+    private Rigidbody2D RB;
+    private bool left;
+    public float coins = 0;
+    public AudioClip coin;
+    private AudioSource aso;
+    public AudioClip wolf;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+        aso = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (RB.velocity.x > 0.05) { left = false; anim.SetBool("Left", false); }
+        if (RB.velocity.x < -0.05) { left = true; anim.SetBool("Left", true); }
+
+        
+
+        if (Mathf.Abs(RB.velocity.x) > 0.1 || Mathf.Abs(RB.velocity.x) < -0.1) { anim.SetFloat("Speed", 1.0f);}
+        if (Mathf.Abs(RB.velocity.x) <= 0.1 && Mathf.Abs(RB.velocity.x) >= -0.1) { anim.SetFloat("Speed", 0f); }
+
+        if (Mathf.Abs(RB.velocity.x) > 1.5 || Mathf.Abs(RB.velocity.x) < -1.5) { anim.SetFloat("Speed", 2.0f); }
+        if (Mathf.Abs(RB.velocity.x) <= 1.5 && Mathf.Abs(RB.velocity.x) >= -1.5 && Mathf.Abs(RB.velocity.x) > 0.1 && Mathf.Abs(RB.velocity.x) < -0.1) { anim.SetFloat("Speed", 1f); }
+
+        if (Mathf.Abs(RB.velocity.y) <= 0.05) { anim.SetFloat("SpeedY", 0f); }
+        if (Mathf.Abs(RB.velocity.y) > 0.05) { anim.SetFloat("SpeedY", 1f); }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+
+            Destroy(collision.gameObject);
+            if (coins == 2)
+            {
+                anim.SetBool("MegaWolf", true);
+            }
+            coins++;
+            CoinSound();
+
+            
+
+
+        }
+
+        if (collision.gameObject.CompareTag("End"))
+        {
+
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            WolfSound();
+
+
+        }
+
+        if (collision.gameObject.CompareTag("Enemie") && coins == 3)
+        {
+
+            Destroy(collision.gameObject);
+            CoinSound();
+            
+
+
+        }
+
+        if (collision.gameObject.CompareTag("Enemie") && coins < 3)
+        {
+
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            WolfSound();
+
+
+        }
+    }
+
+    public void CoinSound() {
+
+        aso.clip = coin;
+        aso.Play();
+
+    }
+
+    public void WolfSound() {
+
+        aso.clip = wolf;
+        aso.Play();
+    }
+
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float CharacterVelocity = 2.00f;
+    public float Force = 5.00f;
+    public Sprite izquierda;
+    public Sprite derecha;
+    public Sprite paradopos;
+    public Sprite paradoneg;
+    public Sprite cayendoneg;
+    public Sprite cayendopos;
+    public Sprite saltandoneg;
+    public Sprite saltandopos;
+    public Animation walking;
+
+    public GameObject character;
+    private Rigidbody2D RBCharacter;
+    private Camera camera;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+
+        if (character)
+        {
+            RBCharacter = character.GetComponent<Rigidbody2D>();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RBCharacter.transform.eulerAngles = new Vector3(0, 0, 0);
+        camera.transform.position = new Vector3(character.transform.position.x, character.transform.position.y, -10);
+        if (RBCharacter && Input.GetKeyDown("space") && Mathf.Abs(RBCharacter.velocity.y) <= 0.05f)
+        {
+
+            RBCharacter.AddForce(new Vector2(0, Force), ForceMode2D.Impulse);
+
+        }
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (RBCharacter) {
+
+            RBCharacter.AddForce(new Vector2(Input.GetAxis("Horizontal") * CharacterVelocity, 0));
+
+        }
+
+
+    }
+
+}
